de6482eb0564dda421398133dd97da91b3cc5cab
Camilo Acuña <camilo.aku@gmail.com>
2022-12-22 17:14:01 +0700

    feat(icon): add icons components

---
+++ frontend/src/components/icon/index.tsx
@@ -0,0 +1,7 @@
+import KubernetesIcon from "./kubernetes";
+import AWSIcon from "./aws";
+import AzureIcon from "./azure";
+import DigitalOceanIcon from "./digital-ocean";
+import GoogleCloudIcon from "./google-cloud";
+
+export { KubernetesIcon, AWSIcon, AzureIcon, DigitalOceanIcon, GoogleCloudIcon };
